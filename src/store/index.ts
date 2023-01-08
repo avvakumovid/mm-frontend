23328337f07330d33255b841aaadda1ffea24b90
avvakumovid <avvakumovid@gmail.com>
2023-01-08 14:25:20 +0300

    refactor: remove unused code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userApi } from './features/userApi';
-import { authApi } from './features/authApi';
 
 const store = configureStore({
     reducer: {
         [userApi.reducerPath]: userApi.reducer,
-        [authApi.reducerPath]: authApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([userApi.middleware, authApi.middleware])
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([userApi.middleware,])
 })
 
 export type RootState = ReturnType<typeof store.getState>
